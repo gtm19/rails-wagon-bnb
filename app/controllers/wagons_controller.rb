5ec354d64b581e2bae81a4b00599cd72c49166a5
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2022-02-01 15:59:50 +0000

    turn off auth for index and show wagons

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -1,14 +1,16 @@
 class WagonsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
+  end
+
+  def show
   end
 
   def new
   end
 
   def create
-  end
-
-  def show
   end
 
   def edit
